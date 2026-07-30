850f4d945b3b0f3f82e2adab1b3575d576c59be0
MohammadALi Salari <ma.slri2128@gmail.com>
2026-07-31 02:10:20 +0330

    start the firs EDA

---
+++ docs/preprocessing_decisions.md
@@ -0,0 +1,24 @@
+## Decision 01
+Remove exact duplicate rows.
+
+## Decision 02
+Keep duplicate review texts.
+
+## Decision 03
+Use 5-class classification.
+
+## Decision 04
+No empty reviews found.
+
+## Decision 05
+Most reviews are shorter than 316 words.
+A Transformer model with max_length=512 is expected to cover almost all samples.
+
+## Decision 06
+Remove HTML tags.
+
+## Decision 07
+Remove URLs.
+
+## Decision 08
+Convert all text to lowercase.
--- src/data_loader.py
+++ src/data_loader.py
@@ -7,21 +7,31 @@ from src.config import IS_KAGGLE
 from src.config import KAGGLE_INPUT
 
 
-def load_reviews():
+def load_train_reviews():
 
     if IS_KAGGLE:
         path = KAGGLE_INPUT / "YOUR_DATASET_NAME" / "reviews_train.csv"
     else:
-        path = RAW_DATA_DIR / "reviews_train.csv"
+        path = RAW_DATA_DIR / "train_data.csv"
 
     return pd.read_csv(path)
 
 
-def load_products():
+def load_test_reviews():
+
+    if IS_KAGGLE:
+        path = KAGGLE_INPUT / "YOUR_DATASET_NAME" / "reviews_test.csv"
+    else:
+        path = RAW_DATA_DIR / "test_data.csv"
+
+    return pd.read_csv(path)
+
+
+def load_title_brand():
 
     if IS_KAGGLE:
         path = KAGGLE_INPUT / "YOUR_DATASET_NAME" / "products.csv"
     else:
-        path = RAW_DATA_DIR / "products.csv"
+        path = RAW_DATA_DIR / "title_brand.csv"
 
     return pd.read_csv(path)
